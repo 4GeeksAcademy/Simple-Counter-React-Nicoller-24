1cffbd8b54c3d74becee0d6082aa11264059cc2b
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-08-29 01:18:58 +0000

    Error solucionado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SecondsCounter from "./secondscounter";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 
 
@@ -20,7 +20,7 @@ const Home = (props) => {
 			<div className="card text-center mb-3" style={{width: "10rem", height: "12rem", backgroundColor: "#1e1c1c", marginLeft:"2%"}}>
 					<div className="card-body">
 						<h5 className="card-title mt-3 text-white" style={{fontSize: "90px"}}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+								<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
 									<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
 									<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
 								</svg>
